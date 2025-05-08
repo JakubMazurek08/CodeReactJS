37346555388a15a4aca36797f92574b418ba6d7b
Kuba L. <u43_jakles_lbn@technischools.com>
2025-05-08 05:09:44 +0200

    update

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -6,7 +6,7 @@ export const Navbar = () => {
         <nav className={'bg-white fixed border-b-2 w-screen h-16 border-green justify-between items-center px-[100px] flex z-50'}>
             <Link to={'/'}><h1 className={'font-bold text-[32px]'}><span className='text-green'>Job</span><span className={'text-blue'}>Prep</span><span className={'text-black'}>AI</span></h1></Link>
             <div className={"h-full items-center gap-20 hidden md:flex"}>
-                <Link to={'/'}><Text type={'h4'}>Find Job</Text></Link>
+                <Link to={'/home'}><Text type={'h4'}>Find Job</Text></Link>
                 <Link to={'/'}><Text type={'h4'}>About</Text></Link>
                 <Link to={'/'}><Text type={'h4'}>Our Team</Text></Link>
                 <Link to={'/user'} className="ml-4">
